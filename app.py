c3536c7f01f69792f222f6f7e6cbbd62218edb09
mujahid ul islam <117376282+Mujahid4321@users.noreply.github.com>
2024-04-02 15:21:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,20 @@
-import os 
 from flask import Flask
 from sqlalchemy import create_engine
+import os  # Import the os module for accessing environment variables
 
 app = Flask(__name__)
 
 # Fetch database connection details from environment variables
 db_host = os.getenv('DB_HOST')
-db_port = os.getenv('DB_PORT')
+db_port_str = os.getenv('DB_PORT')
+db_port = int(db_port_str) if db_port_str and db_port_str.isdigit() else None
 db_name = os.getenv('DB_NAME')
 db_user = os.getenv('DB_USER')
 db_password = os.getenv('DB_PASSWORD')
+
+# Check if DB_PORT is None or not a valid integer
+if db_port is None:
+    raise ValueError("DB_PORT environment variable is not set or is not a valid integer.")
 
 # Create a connection string
 db_url = f'postgresql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}'
